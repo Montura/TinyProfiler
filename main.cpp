b3989e2739e7e66c995feb9d3cf02de9dbd03223
Andrey Mikhalev <andrey.mikhalev@delightex.com>
2021-01-19 01:40:20 +0300

    Working on miniprofiler for Windows

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "profiler.h"
+
+uint64_t add(int x, int y) {
+  uint64_t res = 0;
+  for (int i = 0; i < 1e5; ++i) {
+    res = x * y;
+  }
+  return res;
+}
+
+int main() {
+  MiniProfiler pProfiler;
+
+  for (int i = 0, j = 1; i < 1e3 && j < 1e3; ++i, j += 2) {
+    add(i, j);
+  }
+
+  return 0;
+}
